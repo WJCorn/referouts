f8dbad3ebdf1f82ca31c34cb5f12bc5473c32699
WJCorn <154237332+WJCorn@users.noreply.github.com>
2025-05-11 14:51:43 -0400

    Edits

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,11 @@
-// React DOM render
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Tailwind if used
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
