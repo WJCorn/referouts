be128d4a831e0eb5a0a0bc84da07f7effb035fe6
WJCorn <154237332+WJCorn@users.noreply.github.com>
2025-05-18 09:45:50 -0400

    Update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import App from './pages/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+    <App />
   </React.StrictMode>
 );
