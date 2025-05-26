b1893eaab28fdc25e7c87c7d03e26592f839d700
WJCorn <154237332+WJCorn@users.noreply.github.com>
2025-05-26 15:42:53 -0400

    Link refs fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer className="text-center text-sm text-gray-500 py-10 border-t bg-white dark:bg-gray-900 dark:text-gray-400">
       <div className="space-x-4">
-        <Link to="/privacy-policy" className="hover:underline">Privacy Policy</Link>
-        <Link to="/contact" className="hover:underline">Contact</Link>
-        <Link to="/terms-of-service" className="hover:underline">Terms of Service</Link>
+        <Link to="/PrivacyPolicy" className="hover:underline">Privacy Policy</Link>
+        <Link to="/Contact" className="hover:underline">Contact</Link>
+        <Link to="/TermsOfService" className="hover:underline">Terms of Service</Link>
       </div>
       <p className="mt-2">&copy; {new Date().getFullYear()} Referouts, Inc.</p>
     </footer>
